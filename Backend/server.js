cbe7211059464c13d9874aa08acce7b91ce1012a
Joel Riba <joel.irungu00@gmail.com>
2025-07-05 17:10:41 +0300

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -8,13 +8,6 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // Needed for USSD form POST
 
-// Health check or test endpoint
-app.post('/api/ussd', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to USSD API',
-        status: 'success'
-    });
-});
 
 // USSD routes
 app.use('/ussd', router);
