323e0bcfe017925a3ec72d41dcd6090ae3047ff2
Joel Riba <joel.irungu00@gmail.com>
2025-07-05 17:44:21 +0300

    first commit

--- Backend/Controllers/ussdController.js
+++ Backend/Controllers/ussdController.js
@@ -1,29 +1,24 @@
 const express = require('express');
 
 const handleUSSDRequest = (req, res) => {
-    const { sessionId, serviceCode, phoneNumber, text } = req.body;
+    let { sessionId, serviceCode, phoneNumber, text } = req.body;
+    serviceCode = serviceCode.trim();  
+    console.log('📥 Incoming USSD Request:', req.body);
   
     let response = '';
   
-    // Split the text input by '*' to get menu selections
     const textArray = text.split('*');
   
     if (text === '') {
-        console.log('📥 Incoming USSD Request:', req.body);
-      // This is the first request. Show the main menu
       response = `CON Welcome to MyService
   1. Check Balance
   2. Buy Airtime
   3. Exit`;
     } else if (text === '1') {
-      // User selected "Check Balance"
       response = `END Your balance is KES 500`;
-      
     } else if (text === '2') {
-      // User selected "Buy Airtime"
       response = `CON Enter amount to buy:`;
     } else if (textArray[0] === '2' && textArray.length === 2) {
-      // User entered amount to buy airtime
       const amount = textArray[1];
       response = `END You have successfully purchased KES ${amount} airtime.`;
     } else if (text === '3') {
